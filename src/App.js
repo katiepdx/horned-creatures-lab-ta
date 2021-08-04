0ca9bd7767d17412608fb618cca415d2d1974485
Katie <61799774+katiepdx@users.noreply.github.com>
2021-08-03 17:12:26 -0700

    refactor dropdown and add second filter

--- src/App.js
+++ src/App.js
@@ -7,28 +7,37 @@ import creaturesData from './data/creatures-data';
 class App extends Component {
    state = {
     creatures: creaturesData,
-    filter: 'All'
+    filter: 'All',
+    keyword: 'All'
   }
 
   handleFilterChange = (e) => {
     this.setState({ filter: e.target.value})
   }
+  handleKeywordChange = (e) => {
+    this.setState({ keyword: e.target.value})
+  }
 
   render() {
+    const horns = ['All', 1, 2, 3, 100]
+    const HornsDropdownOptions = horns.map(hornAmount => <option value={hornAmount} key={hornAmount}>{hornAmount}</option>)
+    
+    const keywords = ['All', 'narwhal', 'rhino', 'unicorn', 'unilego', 'triceratops', 'markhor', 'mouflon', 'addax', 'chameleon', 'lizard', 'dragon']
+    const KeywordDropdownOptions = keywords.map(keyword => <option value={keyword} key={keyword}>{keyword}</option>)
+
     const filteredCreatures = this.state.creatures.filter((creature) => 
-      this.state.filter === 'All' || Number(this.state.filter) === creature.horns
-    )
+    (this.state.filter === 'All' || Number(this.state.filter) === creature.horns) && 
+    (this.state.keyword === 'All' || this.state.keyword === creature.keyword))
 
     return (
       <div className="App">
           <Header/>
 
           <select onChange={this.handleFilterChange}>
-            <option value='All'>All</option>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={100}>100</option>
+            {HornsDropdownOptions}
+          </select>
+          <select onChange={this.handleKeywordChange}>
+          {KeywordDropdownOptions}
           </select>
 
           <ImageList creatures={filteredCreatures}/>
